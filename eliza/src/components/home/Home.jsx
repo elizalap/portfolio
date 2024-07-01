af1eea86909fb5076c6b6af6107843e72f4a0bd6
Eliza Lima <eliza.paoliello@soulasalle.com.br>
2024-07-01 20:01:34 -0300

    home component - shapes

--- eliza/src/components/home/Home.jsx
+++ eliza/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import profileImg from '../../assets/profile-img.svg'
 import shapeOne from '../../assets/shape-1.png';
 import shapeTwo from '../../assets/shape-2.png';
 
-import{ FaTwitter, FaGithub } from 'react-icons/fa'
+import{ FaLinkedinIn, FaGithub } from 'react-icons/fa'
 import './home.css';
 
 const Home = () => {
@@ -11,7 +11,7 @@ const Home = () => {
     <section className="home" id="home">
         <div className="home__container container">
             <p className="home__subtitle text-css">
-                Hello, <span>My Name Is</span>
+                Hello, <span>I am</span>
             </p>
             <h1 className="home__title text-cs"><span>ELIZA</span> LIMA</h1>
             <p className="home__job">
@@ -21,7 +21,18 @@ const Home = () => {
             <div className="home__img-wrapper">
               <div className="home__banner">
                 <img src={profileImg} alt="" className="home__profile"/>
+                <img src={shapeOne} alt="" className="shape shape__1" />
+                <img src={shapeTwo} alt="" className="shape shape__2" />
+                <img src={shapeTwo} alt="" className="shape shape__3" />
               </div>
+              <p className="home__data home__data-one">
+                <span className="text-lg">
+                  12 <b>+</b>
+                </span>
+                <span className="text-sm text-cs">
+                  Years of <span>Experience</span>
+                </span>
+              </p>
             </div>
 
             <p className="home__text">
@@ -29,10 +40,10 @@ const Home = () => {
             </p>
 
             <div className="home__socials">
-              <a href="" className="home__social-link">
-                <FaTwitter/>
+              <a href="http://www.linkedin.com/in/elizapaoliello" target="_blank" className="home__social-link">
+                <FaLinkedinIn/>
               </a>
-              <a href="" className="home__social-link">
+              <a href="http://www.github.com/elizalap" target="_blank" className="home__social-link">
                 <FaGithub/>
               </a>
             </div>
